c507bae691ec1d09e4604eb23b9a265e6e69a440
墨羽 <3273095899@qq.com>
2025-03-20 16:41:54 +0800

    1

---
+++ 动态规划/70.爬楼梯.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：pythonProject2 
+@File    ：70.爬楼梯.py.py
+@IDE     ：PyCharm 
+@Author  ：苜苜的烂笔头
+@Date    ：2025/3/20 16:07 
+'''
+class solution:
+
+    def climbstep(self,n:int) -> int:
+        a,b = 1,1
+        for _ in range(n-1):
+            a,b=b,a+b
+        return b
+if __name__=='__main__':
+    ##方法一：创建实例，要不然无法引用类里面的方法
+    sol = solution()
+    a = int(input("输入要达到第几阶台阶"))
+
